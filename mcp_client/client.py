599beaf67f1d237b39c4566ff17f2fc3a734d6ce
Aditya Mishra <adityaamishraaa11@gmail.com>
2025-12-27 23:34:04 +0530

    Update client.py

--- mcp_client/client.py
+++ mcp_client/client.py
@@ -40,13 +40,25 @@ async def main():
     llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
     llm_with_tools = llm.bind_tools(tools)
 
-    prompt = "Draw a tringle rotating in place using the manim tool"
+    prompt = "Roll a dice for me"
     response = await llm_with_tools.ainvoke(prompt)
 
+    #agar tool_call attricute nahi hai toh response return kr do wrna agar hai toh aage ka code execute kro
     if not getattr(response, "tool_calls", None):
         print("\nLLM Reply:", response.content)
         return
+    
+    # selected_tool = response.tool_calls[0]["name"]
+    # selected_tools_args = response.tool_calls[0]["args"]
+    # selected_tool_id = response.tool_calls[0]["id"]
 
+    # tool_result = await named_tools[selected_tool].ainvoke(selected_tools_args)
+
+    # tool_message = ToolMessage(content=tool_result, tool_call_id=selected_tool_id)
+    # final_response = await llm_with_tools.ainvoke([prompt, response, tool_message])
+    # print(f"Final response: {final_response.content}")
+
+    #isse agar tools ko further tools ki reuiqremnt hogi toh woh call klr dega
     tool_messages = []
     for tc in response.tool_calls:
         selected_tool = tc["name"]
